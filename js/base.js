3ee8fa16fab2b7f1b32f05122b0070d0052abf7e
qssss <13720126747@163.com>
2019-05-12 18:51:58 +0800

    新增

---
+++ js/base.js
@@ -0,0 +1,25 @@
+$(function() {
+    //图书浏览
+    $('.page01 .catetitle').xs999(6,{'color':'#adb6e3'});
+    $(".page01 .booklist li").xs999(6,{'color':'#adb6e3'});
+    $(".page01 .bookcate li").xs999(6);
+    
+    //图书检索列表
+    $(".page06 .search_box .search_btn").xs999(6);
+    $(".page06 .search_list li").xs999(6);
+    
+    //图书排行
+    $('.page04 .catetitle').xs999(6,{'color':'#adb6e3'});
+    $(".page04 .booklist li").xs999(6);
+    $(".page04 .bookcate li").xs999(6);
+
+    //新书推荐tab
+   
+
+    //图书详情
+    $(".page02 .BookDetails .title").xs999(6,{'color':'#fff'});
+    $(".page02 .BookDetails .btn_more").xs999(6,{'color':'#fff'});
+    
+    //弹窗
+    $('.page05 .block').xs999(6);
+})
